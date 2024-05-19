079f5744dd28745e3bad1a9eaa413f55c5c3bf37
sumitbhardwajjj <135961691+sumitbhardwajjj@users.noreply.github.com>
2024-05-19 15:33:34 +0530

    commit this

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
 import '../styles/Contact.css'
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 const Contacts= () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_qhpsjxq', 'template_wnyh7sr', form.current, {
+        publicKey: 'Bd-JpcsGogpL0CvkN',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
   return (
     <div className='details'>
       <h1>Contact Me</h1>
       <h3>Please fill out the form below to dicuss the any work opportunity</h3>
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <div className='form'>
-        <input className='input' type="text" placeholder='Your Name' />
+        <input className='input' type="text" placeholder='Your Name' name="user_name" />
         </div>
         <div className='form'>
-        <input className='input' type="text" placeholder='Your Address' />
+        <input className='input' type="email" placeholder='Your email' name="user_email" />
         </div>
         <div className='form'>
-        <textarea className='input'  rows="6" cols="30" placeholder='Your message' />
+        <textarea className='input'  rows="6" cols="30" placeholder='Your message' name="message" />
         </div>
         <div className='form'>
             <button className='submit'>Submit</button>
